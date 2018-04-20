462b4f58571b4ecba3e95dcf05486e265a29f338
alex0-0 <alexande2008@gmail.com>
2018-04-20 18:17:32 -0400

    change pictures and add tensorflow to extract template object from image

--- app/src/main/java/com/example/alex/reemersiondemo/imgmatching/PicMatching.java
+++ app/src/main/java/com/example/alex/reemersiondemo/imgmatching/PicMatching.java
@@ -9,21 +9,26 @@ import android.widget.AdapterView;
 import android.widget.GridView;
 
 import com.example.alex.reemersiondemo.R;
+import com.example.alex.reemersiondemo.record.TensorFlowMultiBoxDetector;
 
 import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.LoaderCallbackInterface;
 import org.opencv.android.OpenCVLoader;
 import org.opencv.android.Utils;
 import org.opencv.core.Mat;
+import org.opencv.core.Rect;
 import org.opencv.imgcodecs.Imgcodecs;
+import org.tensorflow.contrib.android.TensorFlowInferenceInterface;
 
 import java.io.IOException;
 import java.util.ArrayList;
 
 public class PicMatching extends Activity {
     private static String TAG = "PicMatching";
+    private static final String                     MODEL_PATH = "file:///android_asset/ssd_mobilenet_v1_android_export.pb";
     private ArrayList<Mat> imageList;
     private GridView gridView;
+    private TensorFlowMultiBoxDetector tfDetector;
 
     //necessary operation after application load openCV library
     private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(this) {
@@ -49,6 +54,9 @@ public class PicMatching extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pic_matching);
         gridView = findViewById(R.id.pic_matching);
+        TensorFlowInferenceInterface tensorflow = new TensorFlowInferenceInterface(getAssets(), MODEL_PATH);
+        tfDetector = TensorFlowMultiBoxDetector.getInstance();
+        tfDetector.setTensorflow(tensorflow);
         gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             public void onItemClick(AdapterView parent, View v, int position, long id) {
                 if (position == 0)
@@ -78,6 +86,7 @@ public class PicMatching extends Activity {
 
             try {
                 Mat template = Utils.loadResource(getApplicationContext(), R.drawable.template, Imgcodecs.CV_LOAD_IMAGE_COLOR);
+                template = extractTemplate(template);
                 imageList.add(template);
                 imageList.add(Utils.loadResource(getApplicationContext(), R.drawable.a, Imgcodecs.CV_LOAD_IMAGE_COLOR));
                 imageList.add(Utils.loadResource(getApplicationContext(), R.drawable.b, Imgcodecs.CV_LOAD_IMAGE_COLOR));
@@ -92,6 +101,21 @@ public class PicMatching extends Activity {
                 Log.e(TAG, Log.getStackTraceString(e));
             }
         }
+    }
+
+    /**
+     * Extract object from image by TensorFlow
+     * @param image
+     * @return objectImage. If unrecognizable, return original image.
+     */
+    private Mat extractTemplate(Mat image) {
+        ArrayList<Rect> boundRect = tfDetector.recognizeImage(image);
+
+        //assume image only contains template
+        if (boundRect.size() > 0) {
+            return new Mat(image, boundRect.get(0));
+        }
+        return image;
     }
 
     private void match(int pos) {
